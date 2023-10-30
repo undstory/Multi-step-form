324e3e8636410f6806682e07ae0cf7e1deb8a153
Aga Undrul <undrul.undrul@gmail.com>
2023-10-30 09:02:14 +0100

    created thankyou page

--- src/components/ThankYou/index.tsx
+++ src/components/ThankYou/index.tsx
@@ -1,5 +1,33 @@
-const ThankYou: React.FC = () => (
-    <>Thank you</>
+import Container from "../../ui/atoms/Container";
+import Text from "../../ui/atoms/Text";
+import { withMediaQueries } from "../../ui/hoc/withMediaQueries";
+import icon from "../../assets/images/icon-thank-you.svg"
+import { CustomText } from "./style";
+
+type ThankYouProps = {
+    mediaIsPhone?: boolean;
+}
+
+const ThankYou: React.FC<ThankYouProps> = ({ mediaIsPhone }) => (
+    <Container
+        flexdirection="column"
+        justifycontent="center"
+        alignitems="center"
+        width="100%"
+        padding="30px 0"
+    >
+        <img src={icon} alt="thank you icon" />
+        <Text
+            size="22px"
+            weight="bold"
+            color="marineBlue"
+            marginbottom="10px"
+        >Thank you!</Text>
+        <CustomText
+            size={!mediaIsPhone ? "12px" : "15px"}
+        >Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
+        </CustomText>
+    </Container>
 )
 
-export default ThankYou;
+export default withMediaQueries(ThankYou);
---
+++ src/components/ThankYou/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import Text from '../../ui/atoms/Text'
+
+export const CustomText = styled(Text)`
+  max-width: 350px;
+  text-align: center;
+`
